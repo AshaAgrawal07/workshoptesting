3a6ca120933b286bc8f08855b36631a0d01efaf8
AshaAgrawal07 <ashaa2@illinois.edu>
2018-02-19 16:54:59 -0600

    added some test cases

--- test/workshop/testing/CommonIngredientsTest.java
+++ test/workshop/testing/CommonIngredientsTest.java
@@ -2,6 +2,13 @@ package workshop.testing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static workshop.testing.CommonIngredients.findCommonIngredients;
+
+import org.junit.Rule;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+
 /**
  * A famous chef has disguised all his recipes
  * By replacing the name of all the ingredients
@@ -20,4 +27,33 @@ public class CommonIngredientsTest {
 	Only one implementation to test:
 	int findCommonIngredients(String[] arr) {
 	 */
+	@Test
+    public void givenTest() {
+	    String[] input = {"abcdde", "baccd", "eeabg"};
+	    String[] output = {"a", "b"};
+	    assertEquals(output, findCommonIngredients(input));
+    }
+
+    @Test
+    public void emptyTest() {
+        String[] input = {"abcdde", "baccd", "eeabg", ""};
+        String[] output = {""};
+        assertEquals(output, findCommonIngredients(input));
+    }
+
+    @Test
+    public void allSameTest() {
+        String[] input = {"abcddge", "bacgced", "eeacbgd"};
+        String[] output = {"a", "b", "c", "d", "e", "g"};
+        assertEquals(output, findCommonIngredients(input));
+    }
+    @Rule
+
+    @Test
+    public void nullTest() {
+	    String[] input = {null, "abc"};
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Null input");
+        findCommonIngredients(input);
+	}
 }
--- test/workshop/testing/SOSCheckerTest.java
+++ test/workshop/testing/SOSCheckerTest.java
@@ -2,6 +2,13 @@ package workshop.testing;
 
 import org.junit.jupiter.api.Test;
 
+import org.junit.Rule;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static workshop.testing.SOSChecker.countChangesGood;
+
 /**
  * This code is used by a emergency radio bouy
  * It listens for strings of repeated SOS sent over the radio
@@ -26,4 +33,31 @@ public class SOSCheckerTest {
 	 * and
 	 * int countChangesBad(String message)
 	 */
+
+	@Test
+	public void oneChangeTest() {
+		assertEquals("has one change 0->A", countChangesGood("SOSSAS"));
+	}
+
+	@Test
+	public void nullTest() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Null input");
+		countChangesGood(null);
+	}
+
+	@Test
+	public void lowerCaseTest() {
+		assertEquals("has one change 0->A", countChangesGood("sossas"));
+	}
+
+	@Test
+	public void randomCaseTest() {
+		assertEquals("has one change 0->A", countChangesGood("SossAs"));
+	}
+
+	@Test
+	public void oneLetter() {
+		assertEquals("has 2 changes", countChangesGood("s"));
+	}
 }
